401633fdc99474372f66371726036d169ad3eeab
zhayangtao <1264214725@qq.com>
2018-03-04 13:41:25 +0800

    代码提交

---
+++ boot_dubbo/src/main/java/com/example/boot_dubbo/BootDubboApplication.java
@@ -0,0 +1,12 @@
+package com.example.boot_dubbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDubboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDubboApplication.class, args);
+	}
+}
